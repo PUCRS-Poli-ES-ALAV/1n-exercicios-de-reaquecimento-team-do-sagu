6e35b337fdca2a6d5c03875ccbbbf73177260703
Gustavo de Freitas Fidelis <gustavo.fidelis@edu.pucrs.br>
2025-08-04 20:28:46 -0300

    adicionando novos exerc resolvidos

--- Main.java
+++ Main.java
@@ -15,5 +15,22 @@ public class Main {
         int somaResultado = calc.soma(a, b);
         System.out.println("Resultado da multiplicação: " + resultado);
         System.out.println("Resultado da soma: " + somaResultado);
+
+
+        System.out.print("Digite o denominador da fração: ");
+        double denominador = in.nextDouble();
+
+        double resultadoFracao = calc.somaFracao( denominador);
+        System.out.println("Resultado da soma da fração: " + resultadoFracao);
+
+        System.out.print("Digite uma string para inverter: ");
+        String str = in.nextLine();
+        in.nextLine();
+        String resultadoInversao = calc.inverteString(str);
+        System.out.println("Resultado da inversão da string: " + resultadoInversao);
+        System.out.println("Digite uma palavra para verificar se é palíndromo: ");
+        String palavra = in.nextLine();
+        boolean resultadoPalindromo = calc.ehPalindrome(palavra);
+        System.out.println("A palavra é um palíndromo? " + resultadoPalindromo);
     }
 }
--- exerc1.java
+++ exerc1.java
@@ -7,14 +7,51 @@ public class exerc1 {
     }
 
     public int soma(int a, int b) {
-        int resultado = 0;
-        if (a > 0 || b < 0 ) {
-            resultado = soma(a + 1, b - 1);
+        if(b == 0) {
+            return a;
+        }if(a == 0) {
+            return b;
         }
-        if (a < 0 || b > 0) {
-            resultado = soma(a - 1, b + 1);
-        }
-        return resultado;
-
+        return soma(a + 1, b - 1);
     }
+
+    public double somaFracao(double denominador){
+        if (denominador == 0) {
+            return 0;
+        }
+        return 1.0 / denominador + somaFracao(denominador - 1);
+    }
+
+    public String inverteString(String str){
+        if (str.isEmpty()) {
+            return str;
+        }
+        return inverteString(str.substring(1)) + str.charAt(0);
+    }
+
+    public int geradorDeSequencia(int n){
+        if(n<=0){
+            return 0;
+        } else if(n == 1) {
+            return 1;
+        } else if(n == 2) {
+            return 1;
+        }
+        return 2*geradorDeSequencia(n-1) + 3*geradorDeSequencia(n-2);
+    }
+
+    public boolean ehPalindrome(String str){
+        int inicio = 0;
+
+        int fim = str.length()-1;
+        if(inicio<fim){
+            if(str.charAt(inicio) != str.charAt(fim)){
+                return false;
+            }
+            return ehPalindrome(str.substring(inicio + 1, fim));
+        } else {
+            return true;
+        }
+    }
+
 }
